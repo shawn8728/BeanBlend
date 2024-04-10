d067deae539e04571292a62eeb791a54af342f15
shawn8728 <wang61107@gmail.com>
2024-04-10 04:03:50 -0700

    Initial Commit

---
+++ BeanBlend/BeanBlendApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeanBlendApp.swift
+//  BeanBlend
+//
+//  Created by Shawn Wang on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BeanBlendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
